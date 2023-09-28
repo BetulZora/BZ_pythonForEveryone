987a4f93fd06f9be6d3b4e4c0d2c19207ceefa82
Betul Zora <zorabtl@gmail.com>
2023-09-28 11:26:57 -0500

    Added three locations from Iowa to where.data

--- geodata/geoload.py
+++ geodata/geoload.py
@@ -12,7 +12,7 @@ api_key = False
 
 if api_key is False:
     api_key = 42
-    serviceurl = "http://py4e-data.dr-chuck.net/json?"
+    serviceurl = "http://py4e-data.dr-chuck.net/python json?"
 else :
     serviceurl = "https://maps.googleapis.com/maps/api/geocode/json?"
 
